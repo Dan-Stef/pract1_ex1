491b5b1f51736c403db0a93c78bfa2403095494d
Даниил Стефаненков <daniilstef2391@gmail.com>
2020-06-10 13:26:17 +0300

    Добавьте файлы проекта.

---
+++ pract1_ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract1_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter triangle side a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter triangle side b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double S = (a + b) / 2;
+            Console.WriteLine("Triangle square is: " + S);
+            Console.ReadKey();
+        }
+    }
+}
